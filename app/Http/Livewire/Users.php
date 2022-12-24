d6bcd0c99a8448a2a19b4841af6e83913dea94dd
Matias Olivera <matu120686@gmail.com>
2022-12-24 13:16:39 -0300

    crea usuario terminado con toaststr

--- app/Http/Livewire/Users.php
+++ app/Http/Livewire/Users.php
@@ -2,12 +2,45 @@
 
 namespace App\Http\Livewire;
 
+
+use App\Models\User;
 use Livewire\Component;
 
 class Users extends Component
 {
+    public $users, $name, $email, $password, $user_id;
+
     public function render()
     {
+        $this->users = User::latest()->get();
+
         return view('livewire.users');
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+            
+           User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => bcrypt($this->password),
+        ]);
+
+        $this->name = '';
+        $this->email = '';
+        $this->password = '';
+
+        //session()->flash('message', 'User Created Successfully.');
+        $this->success('User Created Successfully.');
+    }
+
+    public function success()
+    {
+       $this->dispatchBrowserEvent('alert', [ 'message' => 'Usuario creado']);
     }
 }
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,3 +1,46 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class="container">
+    <h1>Crea nuevos Usuarios</h1>
+    <!-- @if (session()->has('message'))
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
+    @endif -->
+    <form>
+        <div class="mb-3">
+            <label for="">Nombre</label>
+            <input type="text" wire:model="name" class="form-control">
+            @error('name') <em class="error">{{ $message }}</em> @enderror 
+        </div>
+        <div class="mb-3">
+            <label for="">Email</label>
+            <input type="email" wire:model="email" class="form-control">
+            @error('email') <em class="error">{{ $message }}</em> @enderror 
+        </div>
+        <div class="mb-3">
+            <label for="">Password</label>
+            <input type="password" wire:model="password" class="form-control">
+            @error('password') <em class="error">{{ $message }}</em> @enderror
+        </div>
+        <button wire:click.prevent="store()" type="submit" class="btn btn-primary">Guardar</button>
+    </form>
+    <div class="table-primary">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th cope="col">#</th>
+                    <th cope="col">Nombre</th>
+                    <th cope="col">Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
